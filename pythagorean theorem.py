9d199fab8487739baafc7e2cfe4b066fc8accc89
Noburu-os <Jusin.driskell@outlook.com>
2021-04-27 10:41:55 -0500

    Add files via upload

---
+++ pythagorean theorem.py
@@ -0,0 +1,67 @@
+# first import math
+import math
+# this is the formula for the unknown side of a triangle slope 
+# A squared + B Squared = C squared
+# take the known sides of the first triangle and multiply them both squared
+base = 24
+
+height = 7
+## the formula starts here A * A + B * B = C * C
+# feel free to switch the mathmatical symbol
+answer = base * base + height * height
+print('this is the answer without the application of the square root formula')
+print answer
+print
+print
+## square root of the first output number
+print('this is the square root of the answer')
+print math.sqrt(answer)
+print
+print
+print
+### next divide back of triangles
+back_1 = 1
+back_2 = 0.8
+## print them both divided by eachother back_2 first
+print('this is the dividend of both backs')
+dividend = back_2/back_1 
+print dividend
+print
+print
+# copy and paste the output of the square root of "answer" and multiply it by "dividend"
+answer_2 = dividend * 12.0415945788
+print('this should be your final answer not squared')
+print answer_2
+print
+print
+print('this should be your final answer squared')
+print math.sqrt(answer_2)
+## I recommend using the final answer not squared
+#
+#
+# TIP FOR FINDING DISTANCE BETWEEN THINGS 
+# 
+# IN MOST CASSES ADD FINAL NUMBER TO THE SMALLEST OPPERATOR
+#
+# FROM THE BEGGINING!!!!!!!!!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
